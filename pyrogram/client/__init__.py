c23cd874fe088a5719ef72f7e674d11c591ae348
JEEPEO <47288442+Jeepeo@users.noreply.github.com>
2019-04-14 14:49:03 +0530

    Update __init__.py

--- pyrogram/client/__init__.py
+++ pyrogram/client/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from .client import Client
+from .client import Client, ApiKey
 from .ext import BaseClient, ChatAction, Emoji, ParseMode
 from .filters import Filters
 
